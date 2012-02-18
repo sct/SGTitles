e4be2c06c9eb0ae29b7d1d8d674a0fb1c7796f69
Ryan Cohen <sctsnipe@gmail.com>
2012-02-18 04:33:13 -0500

    McMMO integration!

--- src/com/sgcraft/sgtitles/McMMOListener.java
+++ src/com/sgcraft/sgtitles/McMMOListener.java
@@ -1,5 +1,8 @@
 package com.sgcraft.sgtitles;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,6 +11,8 @@ import org.bukkit.event.Listener;
 import com.gmail.nossr50.datatypes.PlayerProfile;
 import com.gmail.nossr50.datatypes.SkillType;
 import com.gmail.nossr50.events.McMMOPlayerLevelUpEvent;
+import com.sgcraft.sgtitles.title.Title;
+import com.sgcraft.sgtitles.title.TitleManager;
 
 public class McMMOListener implements Listener {
 	public static SGTitles plugin;
@@ -21,6 +26,47 @@ public class McMMOListener implements Listener {
 		Player player = event.getPlayer();
 		SkillType skill = event.getSkill();
 		PlayerProfile profile = SGTitles.mcPlugin.getPlayerProfile(player);
-		plugin.logger.info("[DEBUG] MCMMO EVENT! - Player: " + player.getName() + " Skill: " + skill.toString() + " new level: " + profile.getSkillLevel(skill));
+		String titleName = getSkillTitle(skill.toString());
+		
+		Integer levelReq = SGTitles.config.getInt("mcmmo.level-required");
+		String format = SGTitles.config.getString("mcmmo.default-format");
+		String position = SGTitles.config.getString("mcmmo.default-position");
+		
+		if (profile.getSkillLevel(skill).equals(levelReq)) {
+			// See if title already exists
+			Title title = TitleManager.get(titleName.toLowerCase());
+			if (title == null) {
+				String data = format.replace("#skill#", titleName);
+				TitleManager.addTitle(titleName.toLowerCase(), data, position);
+				title = TitleManager.get(titleName.toLowerCase());
+			}
+			PlayerManager.giveTitle(player, titleName.toLowerCase());
+			player.sendMessage("§5[§6SGTitles§5] §fCongratulatons! You have been granted the title: " + titleName.toLowerCase());
+		}
+	}
+	
+	private String getSkillTitle(String skillName) {
+		Map<String,String> skills = new HashMap<String, String>();
+		String titleName = null;
+		
+		skills.put("ACROBATICS", "Ninja");
+		skills.put("ARCHERY", "Marksman");
+		skills.put("AXES", "Viking");
+		skills.put("MINING", "Miner");
+		skills.put("EXCAVATION","Excavator");
+		skills.put("FISHING", "Angler");
+		skills.put("HERBALISM","Farmer");
+		skills.put("REPAIR","Blacksmith");
+		skills.put("SWORDS","Fencer");
+		skills.put("TAMING","Trainer");
+		skills.put("UNARMED","Berseker");
+		skills.put("WOODCUTTING","Lumberjack");
+		
+		
+		if (skills.containsKey(skillName))
+			titleName = skills.get(skillName);
+			
+		
+		return titleName;
 	}
 }
